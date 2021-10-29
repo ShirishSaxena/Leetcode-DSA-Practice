d66183f73b1c7cc8d9c4f168ba0c8faeb30e4606
Shirish Saxena <6762915+ShirishSaxena@users.noreply.github.com>
2021-10-29 21:16:06 +0530

    Create Optimised.java

---
+++ 988. Smallest String Starting From Leaf [Medium]/Optimised.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    String curr = "";
+    public String smallestFromLeaf(TreeNode root) {
+        DFS(root, new StringBuilder(""));
+        return curr;
+    }
+    
+    private void DFS(TreeNode root, StringBuilder s){
+        if(root == null)
+            return;
+        
+        s.insert(0, (char) ('a' + root.val));
+        if(root.left == null && root.right == null){
+            if(curr.length() > 0){
+                if(s.toString().compareTo(curr) < 0)
+                    curr = s.toString();
+            }
+            else
+                curr = s.toString();
+            s.deleteCharAt(0); // backTrack
+            return ;
+        }
+        DFS(root.left, s);
+        DFS(root.right, s);
+        
+        s.deleteCharAt(0); // backTrack
+    }
+}
+
+/*
+Woohoo!, finally I feel like I'm starting to get the gist of Tree and recursion problems.
+Yet again, stringBuilder saves the day!
+
+Runtime: 1 ms, faster than 99.69% of Java online submissions for Smallest String Starting From Leaf.
+Memory Usage: 38.9 MB, less than 68.32% of Java online submissions for Smallest String Starting From Leaf.
+
+*/
