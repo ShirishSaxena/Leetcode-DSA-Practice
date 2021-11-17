d9aca5947eb2875e62c656662708a8d5e96ae0f4
Shirish Saxena <6762915+ShirishSaxena@users.noreply.github.com>
2021-11-17 22:08:35 +0530

    Create 538. Convert BST to Greater Tree [Medium].java

---
+++ 538. Convert BST to Greater Tree [Medium].java
@@ -0,0 +1,30 @@
+// Note: This question is the same as 1038: https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+
+// Although, the question may be same but this time I used iterate approach rather than recursion...
+
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        int max = 0;
+        
+        TreeNode currNode = root;
+        Stack<TreeNode> s = new Stack();
+        
+        while(!s.isEmpty() || currNode != null){
+            // save everything right
+            while(currNode != null){
+                s.push(currNode);
+                currNode = currNode.right;
+            }
+            
+            // do something
+            currNode = s.pop();
+            max += currNode.val;
+            currNode.val = max;
+            currNode = currNode.left;
+        }
+        
+        
+        return root;
+        
+    }
+}
